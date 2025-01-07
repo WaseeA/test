8784958035ac8dc963008c103701e3a4e6f7be82
Wasee <waseealam@protonmail.com>
2025-01-07 04:56:17 +0000

    updated docker build

--- docker-build.sh
+++ docker-build.sh
@@ -4,17 +4,30 @@ set -euo pipefail
 set -x
 
 if [ -f release.properties ]; then
-    VERSION=$(grep 'project.rel.ai.knorket\\:test=' release.properties | cut -d'=' -f2)
+    VERSION=$(grep 'project.rel.ai.knorket\\:trino-nlp-embeddings=' release.properties | cut -d'=' -f2)
 else
     VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
 fi
 TRINO_VERSION=$(mvn help:evaluate -Dexpression=dep.trino.version -q -DforceStdout)
-TAG=WaseeA/test:$VERSION
 
+echo "Using TRINO_VERSION: $TRINO_VERSION"
+echo "Using VERSION: $VERSION"
+
+TAG=waseedockerhub9/test:$VERSION
+
+# FOR DEPLOYMENT
 docker buildx build \
-    --platform linux/amd64,linux/arm64 \
+    --platform linux/amd64 \
     -t "$TAG" \
     --build-arg TRINO_VERSION="$TRINO_VERSION" \
     --build-arg VERSION="$VERSION" \
     --push \
     .
+
+# FOR LOCAL TESTING
+# docker buildx build \
+#     -t "$TAG" \
+#     --build-arg TRINO_VERSION="$TRINO_VERSION" \
+#     --build-arg VERSION=0.55-SNAPSHOT \
+#     --load \
+#     .
